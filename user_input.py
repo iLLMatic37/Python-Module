b71435d01fee9562d01ac120a78d75657169fd92
iLLMatic37 <patrickkinyanjui10632@gmail.com>
2024-11-12 16:06:37 +0300

    week-1 assignment done

---
+++ user_input.py
@@ -0,0 +1,11 @@
+# Get user's name
+name = input("What is your name?: ")
+
+# Get user's age (convert input to integer)
+age = int(input("How old are you?: "))
+
+# Get user's location
+location = input("Where do you live?: ")
+
+# Print personalized message using f-strings
+print("Hello {name}, you are {age} years old and live in {location}.")
